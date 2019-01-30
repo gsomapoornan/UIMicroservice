f4fb945e4e6f61ccdf79c7a6a9904d4cf861a40a
gsomapoornan <ganesh.poorna86@gmail.com>
2019-01-30 07:48:17 +0530

    UI service 1.0

---
+++ src/main/java/com/visualpath/UIMicroservice/controller/UIController.java
@@ -0,0 +1,39 @@
+package com.visualpath.UIMicroservice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UIController {
+   @RequestMapping(value = "/index")
+   public String index() {
+      return "index";
+   }
+   @RequestMapping(value = "/user-accounts")
+   public String showProfilesPage() {
+      return "admin";
+   }
+   @RequestMapping(value = "/self-profile")
+   public String selfProfilesPage() {
+      return "selfprofile";
+   }
+   @RequestMapping(value = "/user-search")
+   public String userSearchPage() {
+      return "userprofiles";
+   }
+   @RequestMapping(value = "/catalog")
+   public String catalogPage() {
+      return "catalog";
+   }
+   
+  /* @GetMapping("/searchuser")
+   public String searchuserForm(Model model) {
+   model.addAttribute("searchuser", new Searchuser());
+       return "searchuser";
+   }
+
+   @PostMapping("/searchuser")
+   public String searchuserSubmit(@ModelAttribute("searchuser") Searchuser searchuser) {
+       return "userprofiles";
+   }*/
+}
